19abadab97828c6702d602fbd4f3d49dfb2d2a10
azerkazimov <648azer@gmail.com>
2025-02-10 00:49:53 +0400

    feat: add Product interface to define product structure with essential properties

---
+++ src/components/helpers/interfaces/products.ts
@@ -0,0 +1,10 @@
+// Product interface
+export interface Product {
+    id: string;
+    category: string;
+    subcategory: string;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+  }
